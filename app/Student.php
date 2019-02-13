157d95d15d9b078a9274b54751be0b3f87e97106
Prem Kumar Singh <premsingh57@gmail.com>
2019-02-13 20:27:50 +0545

    show student changed

--- app/Student.php
+++ app/Student.php
@@ -14,10 +14,15 @@ class Student extends Model
     public function country(){
     return $this->belongsTo('App\Country');
 }
-//    public function classBatchSections()
-//    {
-//        return $this->belongsToMany(ClassBatchSection::class,'class_section_students');
-//    }
+    public function classBatchSections()
+    {
+        return $this->belongsToMany(ClassBatchSection::class,'class_section_students');
+    }
+
+    public function classSections()
+    {
+        return $this->hasMany(ClassSectionStudent::class);
+    }
 
 }
 
