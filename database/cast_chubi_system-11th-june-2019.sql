42b2388f6a3018176e7691f78c25fae0db5fb939
youramesh5@gmail.com <youramesh5@gmail.com>
2019-07-26 09:42:57 +0545

    delete and updated db

--- database/cast_chubi_system-11th-june-2019.sql
+++
@@ -1,8 +0,0 @@
-<div class="error"><h1>Error</h1><p><strong>SQL query:</strong>
-<a href="db_sql.php?sql_query=SET+time_zone+%3D+%22%2B00%3A00%22&amp;show_query=1&amp;db=cast_chubi_system"><span class="nowrap"><img src="themes/dot.gif" title="Edit" alt="Edit" class="icon ic_b_edit" />&nbsp;Edit</span></a>    </p>
-<p>
-<code class="sql"><pre>
-SET time_zone = &quot;+00:00&quot;
-</pre></code>
-</p>
-</div>
